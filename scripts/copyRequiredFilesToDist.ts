84acdee3074974df660d22753375ee5bd4651d7c
御坂云见 <447f@misaka.org>
2025-06-06 13:10:22 +0800

    fix: release script

--- scripts/copyRequiredFilesToDist.ts
+++ scripts/copyRequiredFilesToDist.ts
@@ -1,12 +1,14 @@
 import path from 'path';
 import fs from 'fs';
 
-const packageJsonPath = path.resolve(__dirname, 'package.json');
-const distPackageJsonPath = path.resolve(__dirname, 'dist', 'package.json');
+const cwd = process.cwd();
+
+const packageJsonPath = path.resolve(cwd, 'package.json');
+const distPackageJsonPath = path.resolve(cwd, 'dist', 'package.json');
 console.log('复制 package.json 文件...');
 fs.copyFileSync(packageJsonPath, distPackageJsonPath);
 
-const readmePath = path.resolve(__dirname, 'README.md');
-const distReadmePath = path.resolve(__dirname, 'dist', 'README.md');
+const readmePath = path.resolve(cwd, 'README.md');
+const distReadmePath = path.resolve(cwd, 'dist', 'README.md');
 console.log('复制 README.md 文件...');
 fs.copyFileSync(readmePath, distReadmePath);
